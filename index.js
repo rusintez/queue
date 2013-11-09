e077b1e82c132cb16d6961f1c92d36a59724597a
vlspopov <ykt2002@yandex.ru>
2013-11-09 19:48:05 +0800

    ADD: pass scope to constructor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function Queue() {
   this._queue = [];
+  this._scope = Array.prototype.slice.call(arguments);
   return this;
 }
 
@@ -11,11 +12,13 @@ Queue.prototype.add = function(fn) {
 
 Queue.prototype.run = function(cb) {
   var queue = this._queue;
+  var scope = this._scope;
   var next = function(error) {
     if (error !== void 0) return cb(error);
     var task = queue.shift();  
     if (!task) return cb();
-    task((function(){
+    var s = scope.slice(0);
+    s.push((function(){
       var executed = false;
       return function(error) {
         if (executed) throw new Error('callback can\'t be executed twice');
@@ -23,6 +26,7 @@ Queue.prototype.run = function(cb) {
         next(error);
       }
     })());
+    task.apply(task, s);
   }
   next();
 }
